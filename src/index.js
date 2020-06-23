f558ab2f69658a2e93d67d50f4618b3bd1d40469
Andrew Stanton-Nurse <anurse@github.com>
2020-06-23 10:00:41 -0700

    initial pink theme

--- src/index.js
+++ src/index.js
@@ -11,9 +11,15 @@ const darkTheme = getTheme({
   name: "GitHub Dark",
 });
 
+const pinkTheme = getTheme({
+  style: "pink",
+  name: "GitHub PINK!",
+})
+
 fs.mkdir("./themes", { recursive: true })
   .then(() => Promise.all([
     fs.writeFile("./themes/light.json", JSON.stringify(lightTheme, null, 2)),
     fs.writeFile("./themes/dark.json", JSON.stringify(darkTheme, null, 2)),
+    fs.writeFile("./themes/pink.json", JSON.stringify(pinkTheme, null, 2)),
   ]))
   .catch(() => process.exit(1))
